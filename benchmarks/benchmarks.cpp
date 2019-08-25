9af4b8469e66ca06fd3d5b09051c544c6a45170b
TyRoXx <tyroxxxx@gmail.com>
2019-08-25 07:51:22 +0200

    benchmark for store_blob

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -1,5 +1,7 @@
 #include "in_memory_fuse_frontend_shared/directory.h"
+#include "trees/import.h"
 #include <benchmark/benchmark.h>
+#include <random>
 
 static void benchmark_scan_directory(benchmark::State &state)
 {
@@ -10,5 +12,21 @@ static void benchmark_scan_directory(benchmark::State &state)
 }
 
 BENCHMARK(benchmark_scan_directory)->Unit(benchmark::kMillisecond);
+
+static void benchmark_import(benchmark::State &state)
+{
+    using random_bytes_engine = std::independent_bits_engine<std::mt19937, 8, unsigned char>;
+    random_bytes_engine rbe;
+    std::vector<unsigned char> blob(1024 * 1024 * 10);
+    std::generate(begin(blob), end(blob), std::ref(rbe));
+    for (auto _ : state)
+    {
+        dogbox::sqlite_handle const database = dogbox::open_sqlite(":memory:");
+        dogbox::initialize_blob_storage(*database);
+        dogbox::store_blob(*database, reinterpret_cast<std::byte const *>(blob.data()), blob.size());
+    }
+}
+
+BENCHMARK(benchmark_import)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
